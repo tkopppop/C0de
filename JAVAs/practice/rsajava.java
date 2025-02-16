e9e4dba1fda31a6562402d5ce7bf3085fc7c2470
정.경.주 <156498741+tkopppop@users.noreply.github.com>
2025-02-17 07:27:19 +0900

    Update rsajava.java

--- JAVAs/practice/rsajava.java
+++ JAVAs/practice/rsajava.java
@@ -20,7 +20,7 @@ import java.util.Arrays;
 import javax.crypto.Cipher;
 
 public class RSAjava{
-  public static void main(String[] args){
+  public static void main(String[] Args){
 
   try{
     KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
