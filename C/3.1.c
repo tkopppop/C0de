f2dc029c5490db94247483c768ef224f5bf41d91
정경주 <156498741+tkopppop@users.noreply.github.com>
2025-03-01 07:07:45 +0900

    Create 3.1.c

---
+++ C/3.1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "types.h"
+
+
+int main(void)
+{
+
+  int func1(void)
+  {
+    printf("한글 입니다. 일본인입니다. 한국은 일본을 싫어하는 나라라고 생각하지만 실제로는 운동화 슈즈하는 날이죠\n");
+  }
+
+  s32 (*fnp1)();
+  fnp1 = func1;
+
+  func1();
+
+}
