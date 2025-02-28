6de31dc701dbca3c2de4012da09e58668ba4f7e7
정경주 <156498741+tkopppop@users.noreply.github.com>
2025-03-01 05:22:07 +0900

    Update webserver_selectexample.c

--- C/webserver_selectexample.c
+++ C/webserver_selectexample.c
@@ -1,3 +1,10 @@
+/*
+
+  webserver_selectexample.c
+  - x90c (tf8)
+  - x90cx90c1@gmail.com
+
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -15,6 +22,7 @@
 #define MAX_CLIENTS     10
 
 
+/* processing client request */
 void handle_client(int client_sock) {
     
   const char* response = "HTTP/1.1 200 OK\r\n"
@@ -43,7 +51,7 @@ void handle_client(int client_sock) {
 
 }
 
-
+/* entry point */
 int main(int argc, char** argv) {
   s32 server_sock;
   s32 client_sock;
@@ -86,15 +94,18 @@ int main(int argc, char** argv) {
 
   printf("listen server port:%d\n", PORT);
 
+  /* inf */
   while (1) {
 
     read_fds = master_fds;
 
+    /* select */
     if (select(max_fd + 1, &read_fds, NULL, NULL, NULL) == -1) {
       perror("select");
       exit(EXIT_FAILURE);
     }
 
+    /* mon fds */
     for (fd = 0; fd <= max_fd; fd++) {
 
       if (FD_ISSET(fd, &read_fds)) {
@@ -122,3 +133,8 @@ int main(int argc, char** argv) {
   close(server_sock);
   return 0;
 }
+
+/* EoC */
+
+
+
