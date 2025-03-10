7a1a41e88b3c5f71aa14f49d7031239d87e23719
정경주 <156498741+tkopppop@users.noreply.github.com>
2025-03-10 21:00:02 +0900

    Update 3.1.c

--- C/3.1.c
+++ C/3.1.c
@@ -8,7 +8,7 @@ int main(void)
 
   int func1(void)
   {
-    printf("한글 입니다. 일본식 사고의 한국인!인나! 한국은 일본을 싫어하는 나라라고 생각하지만 실제로는 운동화 슈즈하는 날이죠\n");
+    printf("Korea, Japan, Tokyo city, Tokyo, England, Yemen, good kill\n");
   }
 
   func1();
