bc66ad1d3157846c6d19c0e5c56b25672493cd79
정경주 <156498741+tkopppop@users.noreply.github.com>
2025-03-13 04:22:49 +0900

    Update 3.1.c

--- C/3.1.c
+++ C/3.1.c
@@ -9,7 +9,7 @@ int main(void)
   int func1(void)
   {
     printf("tf8 trip: Korea, Japan, Tokyo.\n");
-    printf("code is a life cycle related thinking... and if a talent writing a source code by C she is amazing power. it's not that different\n");
+    printf("code is a life cycle related thinking... and if THEEE talent writing a source code by C she is amazing power. it's not that different\n");
   }
 
   func1();
