7779c12be6e3d5857bc656c45441efbdbd2bfa24
x90c <156498741+tkopppop@users.noreply.github.com>
2025-06-13 22:25:36 +0900

    Update testssssss.c

--- testssssss.c
+++ testssssss.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int formatstringbug_bug2()
 {
-char buf[32]; // bufferoverflow
+char buf[32]; /* bufferoverflow */
 scanf("%s", &buf);
-printf(buf); // formatstringbugfsb.
+printf(buf); /* formatstringbugfsb. */
 }
 int main(void){
 printf("hackit!\n");
