41aef98889bbb691c3f8d4a29e172862fc787783
x90c <156498741+tkopppop@users.noreply.github.com>
2025-06-13 15:08:00 +0900

    Update prologin.c

--- prologin.c
+++ prologin.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-int main(void) {
+void main(void) {
     printf("prologin = v \n\n\n\n\n\n");
 }
+
+#include <stdio.h>
+#include <stdlib.h>
+int main2(void) { }
+
+/* no override, overload */
+
+/****
+void main(void){
+_init();
+}
+****/
+
+#define IF_DEF    "#ifdef"
+#define END_IFDEF "#endif"
+
+IF_DEF _A_ 
+void main(void){ }
+END_IFDEF
+
+IF_DEF _B_
+int main(int argc, char *argv[]){ }
+END_IFDEF
+
