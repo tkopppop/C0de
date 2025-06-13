d70dfd6e7833a9c26a6e7f7f7771dfde5a23afa0
x90c <156498741+tkopppop@users.noreply.github.com>
2025-06-13 22:25:02 +0900

    Update prologin.c

--- prologin.c
+++ prologin.c
@@ -7,7 +7,7 @@ void main(void) {
 #include <stdio.h>
 #include <stdlib.h>
 
-/* no override, overload */
+/* no override, overload(you develop oop some if you wanted to construction of overloading feature of C by C)*/
 
 /****
 void main(void){
