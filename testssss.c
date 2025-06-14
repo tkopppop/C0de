daa0655a2ff10d5213df2cd19154d237a672bb31
x90c <156498741+tkopppop@users.noreply.github.com>
2025-06-14 16:59:36 +0900

    Update testssss.c

--- testssss.c
+++ testssss.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-int meshmang(){//topfunction define
+int meshmang(){/* topfunction define */
 
 }
 
-int main(){ // main brace
-{ // inner area1 
+int main(){ /* main brace */
+{ /* inner area1 */
 int a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z;
 char cc;
 printf("%c\n", cc);
-{ // inner area2
+{ /* inner area2 */
 unsigned long cc=1000;
 printf("test %ld\n", cc);
 }
@@ -16,6 +16,6 @@ printf("test %ld\n", cc);
 char cc;
 printf("%c\n", cc);
 }
-int meshmang_bottleneck(){// breace try 5
+int meshmang_bottleneck(){/* breace try 5 */
 }
 
