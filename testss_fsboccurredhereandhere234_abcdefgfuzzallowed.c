8e65d048964f8925916ffd4fe629f27203a5b7b9
global x90c aka(handle) hacker homepage and git repositories for research and spread files <156498741+tkopppop@users.noreply.github.com>
2025-07-03 04:59:54 +0900

    Delete testss_fsboccurredhereandhere234_abcdefgfuzzallowed.c

--- testss_fsboccurredhereandhere234_abcdefgfuzzallowed.c
+++
@@ -1,6 +0,0 @@
-#include <locale.h>
-#define AAA/*//{}x*/ ("***\n\n\npronformatstrinbugshahahalamme")
-int main(void){setlocale(LC_ALL, "");printf(AAA/*dangerformatstringbugpronwarningsinhereatthereonexploitpossible-securitybulitinsx90csmamawinprogrammerdevelopercoder*/);formatbugstring(10);}
-int formatbugstring(int s){
-  printf("%s\n", s);
-}
