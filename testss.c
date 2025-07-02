76c8f68abb8ea185f1fd870f8a74f22f4da8c5f8
global x90c aka(handle) hacker homepage and git repositories for research and spread files <156498741+tkopppop@users.noreply.github.com>
2025-07-03 05:00:07 +0900

    Delete testss.c

--- testss.c
+++
@@ -1,5 +0,0 @@
-#define AAA/*//{}x*/ ("***\n\n\npronformatstrinbugshahahalamme")
-int main(void){printf(AAA/*dangerformatstringbugpronwarningsinhereatthereonexploitpossible-securitybulitinsx90csmamawinprogrammerdevelopercoder*/);}
-int formatbugstring(int s){
-  printf("%s\n", s);
-}
